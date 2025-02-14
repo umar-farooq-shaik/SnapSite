d98781697fe45290cff4159d96dc0e8e9067adbf
umar-farooq-shaik <umarfarooqshaik645@gmail.com>
2025-02-14 22:05:55 +0530

    SnapSite new project adding convex,,

---
+++ context/MessagesContext.jsx
@@ -0,0 +1,13 @@
+import React, { createContext, useState } from 'react';
+
+export const MessagesContext = createContext();
+
+export const MessagesProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+
+  return (
+    <MessagesContext.Provider value={[messages, setMessages]}>
+      {children}
+    </MessagesContext.Provider>
+  );
+};
